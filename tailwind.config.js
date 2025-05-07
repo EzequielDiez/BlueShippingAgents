1d5f4d82d17405e7a15063eca92b9ddc4f93088b
Ezequiel Diez <ezequieldiez.emd@gmail.com>
2025-05-07 14:53:45 -0300

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,7 @@ export default {
     ],
     theme: {
         screens: {
-            'xs': '481px',
+            'xs': '480px',
             'sm': '640px',
             'md': '768px',
             'lg': '1024px',
