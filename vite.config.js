4aca83014f582eef265fd5af6c208ad4ac0990d9
Ezequiel Diez <ezequieldiez.emd@gmail.com>
2025-05-07 15:30:28 -0300

    Configuración para GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,12 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/BlueShippingAgents/',
   plugins: [
     tailwindcss(),
     react()
   ],
+  build: {
+    outDir: 'docs',
+    base: './'
+  }
 })
