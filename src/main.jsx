28b319e3001c718f6bdc64f49a9ac91040a57040
Ezequiel Diez <ezequieldiez.emd@gmail.com>
2025-05-08 09:59:25 -0300

    fix: set basename for React Router on GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import './styles/globals.css'
+import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename="/BlueShippingAgents/">
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
 )
