d235ff3d79217d4789c7fc89d7c484765f713c18
Ezequiel Diez <ezequieldiez.emd@gmail.com>
2025-05-08 10:08:01 -0300

    fix: remove duplicate BrowserRouter from App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Layout from './components/Layout';
+import { Routes, Route } from 'react-router-dom';
 import Inicio from './components/Inicio';
 import Nosotros from './components/Nosotros';
 import Servicios from './components/Servicios';
@@ -9,15 +8,13 @@ import Contacto from './components/Contacto';
 
 function App() {
     return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<Inicio />} />
-                <Route path="/nosotros" element={<Nosotros />} />
-                <Route path="/servicios" element={<Servicios />} />
-                <Route path="/puertos" element={<Puertos />} />
-                <Route path="/contacto" element={<Contacto />} />
-            </Routes>
-        </Router>
+        <Routes>
+            <Route path="/" element={<Inicio />} />
+            <Route path="/nosotros" element={<Nosotros />} />
+            <Route path="/servicios" element={<Servicios />} />
+            <Route path="/puertos" element={<Puertos />} />
+            <Route path="/contacto" element={<Contacto />} />
+        </Routes>
     );
 }
 
